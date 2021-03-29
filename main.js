3756579aa99e4a18d58acfd7451f0826e94c22dc
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-03-29 18:56:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+// let x = prompt("Выберите ваш язык: ru, en, gr")
+// switch (x){
+//     case "ru":
+//         alert("Вы выбрали русский")
+//     break
+//     case  "en":
+//         alert("You change english language")
+//     break
+//     case "gr":
+//         alert("vdgdgxcbvcvb")
+//     break
+//         default:
+//             alert("Fock U")
+// }
+
+// let x = prompt("Введите число")
+// switch (x==0){
+//     case true:
+//         alert("Верно")  
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// switch (x < 0){
+//     case false:
+//         alert("Верно")
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// switch (x < 0){
+//     case true:
+//         alert("Верно")
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// switch (x < 0){
+//     case false:
+//         alert("Верно")
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// switch (x < 0 || x == 0){
+//     case true:
+//         alert("Верно")
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// switch (x != 0){
+//     case true:
+//         alert("Верно")
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// switch (x){
+//     case "test":
+//         alert("Верно")
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// switch (x ==1 || x == '"1"'){
+//     case true:
+//         alert("Верно")
+//     break;
+//     default:
+//         alert("Неверно")
+//     break;
+// }
+
+// let x = prompt("Введите число")
+// if (x == 0){
+//     alert("Верно")
+// }
+// else{
+//     alert("Неверно")
+// }
+
+// let x = prompt("Введите число")
+// if (x < 0){
+//     alert("Неверно")
+// }
+// else{
+//     alert("Верно")
+// }
